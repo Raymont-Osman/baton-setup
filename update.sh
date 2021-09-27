199cf06616ee4587f4fdfbef206edf3b3e3a2c9e
Digibake <digibake@users.noreply.github.com>
2021-09-27 13:51:03 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,10 @@
 # and install the python requirements
 #==================================
 cd /home/pi/Baton
+sudo supervisorctl stop baton
+cd /home/pi/Baton
 git pull
-git checkout v1.x
-sudo pip3 install -r requirements.txt
+# git checkout v1.x
+git checkout 90b6777
+sudo pip3 install -r requirements.txt 
+sudo supervisorctl start baton
