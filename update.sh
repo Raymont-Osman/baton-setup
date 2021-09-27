4c99bdb4fb2fb5fd647075482e5fd0ccb8d0e677
Digibake <digibake@users.noreply.github.com>
2021-09-27 15:30:03 +0100

    Adding a status report

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ cd /home/pi/Baton
 sudo supervisorctl stop baton
 git pull
 git checkout v1.x
+git pull
+git status
 sudo pip3 install -r requirements.txt 
 sudo supervisorctl start baton
 sudo supervisorctl tail baton
